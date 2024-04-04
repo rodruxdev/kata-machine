8a5cf357f948e02b328c9f63315acc55abe4a2b9
Rodrigo Goitia <rodrigojgq@gmail.com>
2024-04-04 13:40:49 -0300

    Two Crystal Balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump = Math.floor(Math.sqrt(breaks.length));
 
+    let i = 0;
+    for (; i < breaks.length; i = i + jump) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    for (let j: number = i - jump; j < breaks.length && j <= i; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+    return -1;
 }

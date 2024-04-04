317be38f753da5ad010750f55da6722280235684
Rodrigo Goitia <rodrigojgq@gmail.com>
2024-04-04 13:34:51 -0300

    Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    while (lo < hi) {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        if (haystack[m] === needle) {
+            return true;
+        } else if (haystack[m] > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    }
+    return false;
 }

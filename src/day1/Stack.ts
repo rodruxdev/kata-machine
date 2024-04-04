820df2046ecc4bf4a572d143a57fceaf2502dcda
Rodrigo Goitia <rodrigojgq@gmail.com>
2024-04-04 16:41:44 -0300

    Stack implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,41 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+        }
 
-}
+        node.prev = this.head;
+        this.head = node;
+    }
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        const head = this.head;
+        this.head = this.length !== 0 ? head?.prev : undefined;
+
+        head.prev = undefined;
+
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
